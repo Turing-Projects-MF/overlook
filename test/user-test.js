da1b03f9e9337d0eebcf353f88abac6439f031ed
mdflynn <mdflynn34@outlook.com>
2020-11-03 10:35:39 -0700

    Add initial tests for User class and properties

---
+++ test/user-test.js
@@ -0,0 +1,128 @@
+import { expect } from 'chai';
+import User from '../src/User';
+
+describe('User', function() {
+  let user;
+  let users;
+  let bookings;
+  let rooms;
+
+  beforeEach(function() {
+    users = [
+      {
+        "id": 1,
+        "name": "Leatha Ullrich"
+      },
+      {
+        "id": 2,
+        "name": "Rocio Schuster"
+      },
+      {
+        "id": 3,
+        "name": "Kelvin Schiller"
+      },
+      {
+        "id": 4,
+        "name": "Kennedi Emard"
+      }
+    ];
+
+    bookings = [
+      {
+        "id": "5fwrgu4i7k55hl6sz",
+        "userID": 9,
+        "date": "2020/04/22",
+        "roomNumber": 15,
+        "roomServiceCharges": []
+      },
+      {
+        "id": "5fwrgu4i7k55hl6t5",
+        "userID": 43,
+        "date": "2020/01/24",
+        "roomNumber": 24,
+        "roomServiceCharges": []
+      },
+      {
+        "id": "5fwrgu4i7k55hl6t6",
+        "userID": 13,
+        "date": "2020/01/10",
+        "roomNumber": 12,
+        "roomServiceCharges": []
+      },
+      {
+        "id": "5fwrgu4i7k55hl6t7",
+        "userID": 20,
+        "date": "2020/02/16",
+        "roomNumber": 7,
+        "roomServiceCharges": []
+      }
+    ];
+
+    rooms = [
+      {
+        "number": 1,
+        "roomType": "residential suite",
+        "bidet": true,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 358.4
+      },
+      {
+        "number": 2,
+        "roomType": "suite",
+        "bidet": false,
+        "bedSize": "full",
+        "numBeds": 2,
+        "costPerNight": 477.38
+      },
+      {
+        "number": 3,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "king",
+        "numBeds": 1,
+        "costPerNight": 491.14
+      },
+      {
+        "number": 4,
+        "roomType": "single room",
+        "bidet": false,
+        "bedSize": "queen",
+        "numBeds": 1,
+        "costPerNight": 429.44
+      }
+    ];
+
+    user = new User(users, bookings, rooms);
+  });
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+  });
+
+  it('should be an instance of User', function() {
+    expect(user).to.be.an.instanceof(User);
+  });
+
+  it('should only take in arrays as arguments', function() {
+    expect(users, bookings, rooms).to.be.instanceof(Array);
+  });
+
+  it('should contain data for all users', function() {
+    expect(user.users).to.deep.equal(users);
+  });
+
+  it('should contain data for all bookings', function() {
+    expect(user.bookings).to.deep.equal(bookings);
+  });
+
+  it('should contain data for all rooms', function() {
+    expect(user.rooms).to.deep.equal(rooms);
+  });
+
+
+})
+// - searchAvailability
+// - checkoutRoom
+// - deleteReservation - possibly
+// - getTotalSpent
