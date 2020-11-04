ebdaa90e7aed94ecbb804054522b5965410fa2bb
mdflynn <mdflynn34@outlook.com>
2020-11-03 20:34:43 -0700

    Seperate searchAvailability for SRP

--- src/Guest.js
+++ src/Guest.js
@@ -1,10 +1,26 @@
 import User from './User';
 
 class Guest extends User {
-  constructor(users, bookings, rooms, id = null, name = null) {
+  constructor(users, bookings, rooms, currentUser) {
     super(users, bookings, rooms);
-    this.id = id;
-    this.name = name;
+    this.users = users;
+    this.bookings = bookings;
+    this.rooms = rooms;
+    this.currentUser = currentUser;
+  }
+
+  getCurrentBookings() {
+    return this.findGuestsBooking(this.currentUser);
+  }
+
+  filterRoomByType(type, date) {
+    let availableRooms = this.searchAvailability(date);
+    let filteredRooms = availableRooms.filter(room => room.roomType === type);
+    if (!filteredRooms.length) {
+      return (`We are deeply sorry that we do not have any ${type}\s available`);
+    } else {
+      return filteredRooms;
+    }
   }
 }
 
