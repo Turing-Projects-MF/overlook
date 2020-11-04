fa14c252e2148e37b3b2056197d80799bc014cfe
mdflynn <mdflynn34@outlook.com>
2020-11-03 17:30:53 -0700

    Add searchForGuest method

--- src/Manager.js
+++ src/Manager.js
@@ -3,9 +3,16 @@ import User from './User';
 class Manager extends User {
   constructor(users, bookings, rooms) {
     super(users, bookings, rooms);
-
   }
 
+  searchForGuest(name) {
+    let guest = this.users.find(user => user.name === name);
+    let currentBookings = this.bookings.filter(booking => {
+      return booking.userID === guest.id;
+    });
+    let totalSpent = this.calculateTotalSpent(currentBookings);
+    return {guest: guest.name, bookings: currentBookings, spent: totalSpent}
+  }
 }
 
 export default Manager;
