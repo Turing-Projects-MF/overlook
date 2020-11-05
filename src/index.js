545822b57e2ce68f25b82788f5aac64bbf646a22
mdflynn <mdflynn34@outlook.com>
2020-11-05 12:45:35 -0700

    Add login validation and instantiate User based on login

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import './css/base.scss';
 import './css/base.scss';
 import User from './User';
+import Guest from './Guest';
+import Manager from './Manager';
 import apiRequest from './api-request';
 
-var modal = document.getElementById('id01');
+const modal = document.getElementById('id01');
+const bodyLogin = document.querySelector('.body__login');
+const fadeIn = document.querySelector('.fade-in');
+const loginUsername = document.querySelector('#login-username');
+const loginPassword = document.querySelector('#login-password');
+const loginButton= document.querySelector('.login-button');
+const managerDashboard = document.querySelector('.body__manager ');
+const main = document.querySelector('main');
+const guestBookingview = document.querySelectorAll('.body__guest__user__view')[1];
 
 let usersData;
 let roomsData;
 let bookingsData;
 let user;
+let guest;
+let manager;
 
 let recievedUsersData = apiRequest.getUsersData();
 let recievedRoomsData = apiRequest.getRoomsData();
 let recievedBookingsData = apiRequest.getBookingsData();
+
+
+fadeIn.addEventListener('animationend', displayLogin);
+loginButton.addEventListener('click', displayDashboard);
 
 Promise.all([recievedUsersData, recievedRoomsData, recievedBookingsData])
   .then(value => {
@@ -29,7 +45,45 @@ window.onclick = function(event) {
   }
 }
 
-
 function createUser() {
-  user = new User(usersData, bookingsData, roomsData)
+  user = new User(usersData, bookingsData, roomsData);
+}
+
+function displayLogin() {
+  bodyLogin.classList.remove('hidden');
+}
+
+function displayDashboard(e) {
+  e.preventDefault();
+  bodyLogin.classList.add('hidden');
+  if (loginUsername.value.includes('manager')) {
+    managerDashboard.classList.remove('hidden');
+    createManager();
+    console.log(manager);
+  } else if (loginUsername.value.includes('customer')) {
+    main.classList.remove('hidden');
+    guestBookingview.classList.remove('hidden');
+    createGuest();
+  }
+}
+
+function createGuest() {
+  usersData.forEach(user => {
+    if (loginUsername.value === `customer${user.id}` && loginPassword.value === 'overlook2020') {
+      guest = new Guest(usersData, bookingsData, roomsData, user);
+      guest.bookings = createGuestBookings(guest);
+    }
+  })
+}
+
+function createGuestBookings(guest) {
+  return bookingsData.filter(booking => {
+    return booking.userID === guest.currentUser.id;
+  })
+}
+
+function createManager() {
+  if (loginUsername.value === `manager` && loginPassword.value === 'overlook2020')  {
+    manager = new Manager(usersData, bookingsData, roomsData);
+  }
 }
